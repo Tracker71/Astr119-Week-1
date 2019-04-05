fe7a4c073127fcddf892ad3de83a830e50f19f93
Tracker71 <49163009+Tracker71@users.noreply.github.com>
2019-04-04 20:13:33 -0700

    Add files via upload

---
+++ problem 2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 19:47:16 2019
+
+@author: Brady
+"""
+
+#problem 2
+import numpy as np
+total = 0 
+n = [10,50,100,1000]
+for j in range(n)
+for i in range(n+1):
+    term = 8./( (4. *j+1.)*(4. *j+3.))
+    total = total + term
+    
+    print("term:" + str(term))
+print(total)
+print(np.pi-total)
+
